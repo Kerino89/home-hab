6e2301cb4601d0ffe14e41daa739a7ef8d12663e
Anton <kerino89@gmail.com>
2023-09-17 13:15:32 +0300

    feat(config): create storage.config.ts

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { TRPCModule } from "@server/modules/trpc";
+import { DirectoryInfoModule } from "@server/modules/directory-info";
+
+import * as config from "@server/config";
 
 @Module({
-  imports: [TRPCModule],
+  imports: [
+    TRPCModule,
+    DirectoryInfoModule,
+    ConfigModule.forRoot({ isGlobal: true, load: Object.values(config) }),
+  ],
   controllers: [],
   providers: [],
 })
