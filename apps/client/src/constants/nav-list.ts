f0ffc9710b45ce22b6fba6abdeef5df4bfdd2fea
Anton <kerino89@gmail.com>
2023-10-03 19:54:47 +0300

    feat(const): create routes.const

--- apps/client/src/constants/nav-list.ts
+++ apps/client/src/constants/nav-list.ts
@@ -1,10 +1,11 @@
 import React from "react";
 import { BsHdd } from "react-icons/bs";
+import { INTERNAL_ROUTES } from "./routes.const";
 import type { NavItem } from "@client/layouts/base-layout";
 
 export const NAV_LIST: Array<NavItem> = [
   {
-    path: "/disk",
+    path: INTERNAL_ROUTES.DISK,
     icon: React.createElement(BsHdd),
     label: "Диск",
   },
---
+++ apps/client/src/constants/routes.const.ts
@@ -0,0 +1,4 @@
+export const INTERNAL_ROUTES = Object.freeze({
+  DISK: "/disk",
+  LOGIN: "/login",
+});
