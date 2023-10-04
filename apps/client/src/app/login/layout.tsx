20fd4785ddcab031af214168abc36d6b593d8887
Anton <kerino89@gmail.com>
2023-10-04 20:10:05 +0300

    fix(auth-layout): update title

--- apps/client/src/app/login/layout.tsx
+++ apps/client/src/app/login/layout.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { AuthLayout as AuthLayoutComponent } from "@client/layouts/auth-layout";
 
 export default function AuthLayout({ children }: React.PropsWithChildren) {
-  return <AuthLayoutComponent title="title">{children}</AuthLayoutComponent>;
+  return <AuthLayoutComponent title="Home Hub">{children}</AuthLayoutComponent>;
 }
