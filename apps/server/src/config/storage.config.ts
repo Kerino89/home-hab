cbf4d93227817e2f6e845147545965e493f95641
Anton <kerino89@gmail.com>
2023-10-08 20:49:51 +0300

    feat(helpers): create resolveTemp

--- apps/server/src/config/storage.config.ts
+++ apps/server/src/config/storage.config.ts
@@ -1,5 +1,6 @@
 import { registerAs } from "@nestjs/config";
+import { resolveTemp } from "@server/helpers/path";
 
 export default registerAs("storage", () => ({
-  pathStorageFiles: process.env.PATH_STORAGE_FILES || "G:",
+  pathStorageFiles: process.env.PATH_STORAGE_FILES || resolveTemp("disk"),
 }));
--- apps/server/src/helpers/path/path.ts
+++ apps/server/src/helpers/path/path.ts
@@ -1,8 +1,19 @@
-import { normalize, sep } from "node:path";
+import { normalize, sep, resolve } from "node:path";
+import { existsSync, mkdirSync } from "node:fs";
 
 export function validateNamespacePath(rootPath: string, path: string) {
   const normalizeRootPath = normalize(`${rootPath}${sep}`);
-  const normalizePath = normalize(path);
+  const normalizePath = normalize(`${path}${sep}`);
 
   return normalizePath.startsWith(normalizeRootPath);
+}
+
+export function resolveTemp(...args: Array<string>) {
+  const path = resolve(process.cwd(), "..", "..", ".temp", ...args);
+
+  if (!existsSync(path)) {
+    mkdirSync(path, { recursive: true });
+  }
+
+  return path;
 }
