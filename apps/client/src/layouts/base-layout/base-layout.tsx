ecb1afff06e0802aa65fe2767c03deb6377f3d97
Anton <kerino89@gmail.com>
2023-10-08 11:24:22 +0300

    feat(base-layout): add isAuthorized

--- apps/client/src/layouts/base-layout/base-layout.tsx
+++ apps/client/src/layouts/base-layout/base-layout.tsx
@@ -4,11 +4,13 @@ import React from "react";
 import { useProfile } from "@client/services/user";
 import { useAuth } from "@client/services/auth";
 import { isEmpty } from "lodash";
+import { INTERNAL_ROUTES } from "@client/constants/routes.const";
 import { createPathnameWithPublicUrl, removePublicUrl } from "@client/helpers/url";
 import { usePathname } from "next/navigation";
 import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
 import { Layout, Menu, Button, theme } from "antd";
 import { UserInfo } from "./components/user-info";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 import type { BaseLayoutProps } from "./base-layout.interface";
 
@@ -16,8 +18,9 @@ import styles from "./base-layout.module.scss";
 
 export const BaseLayout: React.FC<BaseLayoutProps> = ({ children, navList }) => {
   const { data: user, isLoading } = useProfile();
-  const { logout } = useAuth();
+  const { isAuthorized, logout } = useAuth();
   const [collapsed, setCollapsed] = React.useState(true);
+  const router = useRouter();
   const pathname = usePathname();
   const {
     token: { colorBgContainer },
@@ -40,6 +43,10 @@ export const BaseLayout: React.FC<BaseLayoutProps> = ({ children, navList }) => 
       fullName: [lastName, firstName].filter(Boolean).join(" "),
     };
   };
+
+  if (!isAuthorized) {
+    router.push(INTERNAL_ROUTES.LOGIN);
+  }
 
   return (
     <Layout className={styles.wrapper}>
--- apps/client/src/services/auth/auth.ts
+++ apps/client/src/services/auth/auth.ts
@@ -1,4 +1,5 @@
 import ky from "ky";
+import type { Options as KyOptions } from "ky";
 import { AuthJWT, LoginRequest } from "./auth.interface";
 
 export const login = async (body: LoginRequest): Promise<AuthJWT> => {
@@ -13,8 +14,8 @@ export const logout = async () => {
   return status === 200;
 };
 
-export const refresh = async (): Promise<{ accessToken: string }> => {
-  const response = await ky.post("/api/auth/refresh");
+export const refresh = async (options?: KyOptions): Promise<{ accessToken: string }> => {
+  const response = await ky.post("/api/auth/refresh", options);
 
   return response.json();
 };
