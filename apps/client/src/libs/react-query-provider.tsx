710c70a54a49d912d36400992abec5d59ce11121
Anton <kerino89@gmail.com>
2023-10-08 09:59:30 +0300

    feat: update options QueryClient

--- apps/client/src/libs/react-query-provider.tsx
+++ apps/client/src/libs/react-query-provider.tsx
@@ -1,11 +1,28 @@
 "use client";
 
 import * as React from "react";
+import { HTTPError } from "ky";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export function ReactQueryProvider({ children }: React.PropsWithChildren) {
-  const [queryClient] = React.useState(() => new QueryClient());
+  const { current: queryClient } = React.useRef(
+    new QueryClient({
+      defaultOptions: {
+        mutations: { retry: false },
+        queries: {
+          refetchOnWindowFocus: false,
+          retry(failureCount, error) {
+            if (error instanceof HTTPError && [401, 403].includes(error.response.status)) {
+              return false;
+            }
+
+            return failureCount <= 2;
+          },
+        },
+      },
+    }),
+  );
 
   return (
     <QueryClientProvider client={queryClient}>
