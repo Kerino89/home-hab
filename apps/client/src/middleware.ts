b201e7b39bda6fb6d738583d9cc19606156a520d
Anton <kerino89@gmail.com>
2023-10-15 19:26:51 +0300

    feat(middleware): add redirect /login

--- apps/client/src/middleware.ts
+++ apps/client/src/middleware.ts
@@ -1,9 +1,25 @@
 import { type NextRequest, NextResponse } from "next/server";
+import { INTERNAL_ROUTES } from "@client/constants/routes.const";
 
 export default async function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === "/") {
-    return NextResponse.redirect(new URL("/disk", request.url));
+  const { nextUrl, cookies } = request;
+  const refreshToken = cookies.get("_rt")?.value;
+
+  if (!nextUrl.pathname.startsWith(INTERNAL_ROUTES.LOGIN) && !refreshToken) {
+    return NextResponse.redirect(new URL(INTERNAL_ROUTES.LOGIN, request.url));
   }
+
+  if (nextUrl.pathname === "/") {
+    return NextResponse.redirect(new URL(INTERNAL_ROUTES.DISK, request.url));
+  }
+
+  const response = NextResponse.next();
+
+  if (nextUrl.pathname.startsWith(INTERNAL_ROUTES.LOGIN)) {
+    response.cookies.delete("_rt");
+  }
+
+  return response;
 }
 
 export const config = {
